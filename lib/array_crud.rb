e3f5d7f222f948c0f4e7df086cb01649aaaa4fb9
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-04 18:16:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-<<(["cat", "dog", "fish", "monkey"], "zebra")
+array << "zebra"
 end
 
 def add_element_to_start_of_array(array, element)
